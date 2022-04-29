5e35f1ca0e5d895b7bf1b66dff969d9b9781bbeb
Usofl <90086886+Usofl@users.noreply.github.com>
2022-04-29 19:06:34 +0900

    no message

---
+++ 2_Team/2_Team/Player.cpp
@@ -0,0 +1,140 @@
+#include "stdafx.h"
+#include "Player.h"
+#include "AbstractFactory.h"
+#include "Shield.h"
+#include "ScrewBullet.h"
+#include "GuideBullet.h"
+#include "ObjMgr.h"
+#include "LineMgr.h"
+#include "KeyMgr.h"
+#include "ScrollMgr.h"
+
+CPlayer::CPlayer()
+{
+}
+
+CPlayer::~CPlayer()
+{
+	Release();
+}
+
+void CPlayer::Initialize(void)
+{
+	m_tInfo.fX = 100.f;
+	m_tInfo.fY = 300.f;
+
+	m_tInfo.fCX = 100.f;
+	m_tInfo.fCY = 100.f;
+
+	m_fSpeed = 10.f;
+
+	m_fDiagonal = 100.f;
+
+	m_bJump = false;
+	m_fJumpPower = 15.f;
+	m_fJumpTime = 0.f;
+
+}
+
+int CPlayer::Update(void)
+{
+	if (m_bDead)
+		return OBJ_DEAD;
+
+	// 연산을 진행
+	Key_Input();
+	Jumping();
+	OffSet();
+
+	// 모든 연산이 끝난 뒤에 최종적인 좌표를 완성
+	Update_Rect();
+
+	return OBJ_NOEVENT;
+}
+
+void CPlayer::Late_Update(void)
+{
+	m_tPosin.x = long(m_tInfo.fX + m_fDiagonal * cosf((m_fAngle * PI) / 180.f));
+	m_tPosin.y = long(m_tInfo.fY - m_fDiagonal * sinf((m_fAngle * PI) / 180.f));
+}
+
+void CPlayer::Render(HDC hDC)
+{
+	int		iScrollX = (int)CScrollMgr::Get_Instance()->Get_ScrollX();
+	Rectangle(hDC, m_tRect.left + iScrollX, m_tRect.top, m_tRect.right + iScrollX, m_tRect.bottom);
+
+	// 포신 그리기
+	MoveToEx(hDC, (int)m_tInfo.fX + iScrollX, (int)m_tInfo.fY, nullptr);
+	LineTo(hDC, (int)m_tPosin.x + iScrollX, (int)m_tPosin.y);
+}
+
+void CPlayer::Release(void)
+{
+	
+}
+
+void CPlayer::Key_Input(void)
+{
+	float	fY = 0.f;
+
+	// GetKeyState
+	if (GetAsyncKeyState(VK_LEFT))
+	{
+		m_tInfo.fX -= m_fSpeed;
+	}
+
+	if (GetAsyncKeyState(VK_RIGHT))
+	{
+		m_tInfo.fX += m_fSpeed;
+	}
+
+	if (GetAsyncKeyState(VK_UP))
+		m_tInfo.fY -= m_fSpeed;
+
+	if (GetAsyncKeyState(VK_DOWN))
+		m_tInfo.fY += m_fSpeed;
+
+	if (CKeyMgr::Get_Instance()->Key_Up(VK_SPACE))
+	{
+		m_bJump = true;
+		return;
+	}
+}
+
+void CPlayer::Jumping(void)
+{
+	float		fY = 0.f;
+
+	bool		bLineCol = CLineMgr::Get_Instance()->Collision_Line(m_tInfo.fX, &fY);
+
+	if (m_bJump)
+	{
+		m_tInfo.fY -= m_fJumpPower * m_fJumpTime - 9.8f * m_fJumpTime * m_fJumpTime * 0.5f;
+		m_fJumpTime += 0.2f;
+
+		if (bLineCol && (fY < m_tInfo.fY))
+		{
+			m_bJump = false;
+			m_fJumpTime = 0.f;
+			m_tInfo.fY = fY;
+		}
+	}
+	else if (bLineCol)
+	{
+		m_tInfo.fY = fY;
+	}
+}
+
+void CPlayer::OffSet(void)
+{
+	int		iOffSetX = WINCX >> 1;
+	int		iScrollX = (int)CScrollMgr::Get_Instance()->Get_ScrollX();
+	int		iItv = 300;
+
+
+	if (iOffSetX - iItv > m_tInfo.fX + iScrollX)
+		CScrollMgr::Get_Instance()->Set_ScrollX(m_fSpeed);
+
+	if (iOffSetX + iItv < m_tInfo.fX + iScrollX)
+		CScrollMgr::Get_Instance()->Set_ScrollX(-m_fSpeed);
+}
