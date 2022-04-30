89fe5ddca749c577a8b6b774a3a775f3be5a8f3b
JunkyuSong <sjg9205@gmail.com>
2022-04-30 22:40:59 +0900

    220430 : 실수로 플레이어에 작업

--- 2_Team/2_Team/ObjMgr.cpp
+++ 2_Team/2_Team/ObjMgr.cpp
@@ -4,6 +4,7 @@
 #include "Player.h"
 #include "LineFactory.h"
 #include "BlockFactory.h"
+#include "TrapFactory.h"
 
 CObjMgr* CObjMgr::m_pInstance = nullptr;
 
@@ -36,6 +37,7 @@ void CObjMgr::Initialize(void)
 	m_NotBeing_list[NOTBEING_BLOCK].push_back(CBlockFactory::Create(300, 200));
 	m_NotBeing_list[NOTBEING_BLOCK].push_back(CBlockFactory::Create(350, 300));
 
+	m_NotBeing_list[NOTBEING_TRAP].push_back(CTrapFactory::Create_Thorn());
 
 	for (auto& iterlist : m_NotBeing_list)
 	{
---
+++ 2_Team/2_Team/Thorn.cpp
@@ -0,0 +1,67 @@
+#include "stdafx.h"
+#include "Thorn.h"
+
+
+CThorn::CThorn()
+{
+}
+
+
+CThorn::~CThorn()
+{
+}
+
+void CThorn::Initialize(void)
+{
+	m_tInfo.fX = 50.f;
+	m_tInfo.fY = 300.f;
+	m_tInfo.fCX = 100.f;
+	m_tInfo.fCX = 600.f;
+
+	m_fSpeed = 0.3f;
+}
+
+const int & CThorn::Update(void)
+{
+	// TODO: 여기에 반환 구문을 삽입합니다.
+	m_tInfo.fX += m_fSpeed;
+
+	Update_Rect();
+	return OBJ_NOEVENT;
+
+}
+
+void CThorn::Late_Update(void)
+{
+}
+
+void CThorn::Render(HDC _hDC)
+{
+	int		iScrollX = (int)SCROLLMGR->Get_ScrollX();
+
+	MoveToEx(_hDC, m_tInfo.fX - 50 + iScrollX, m_tInfo.fY - 300, nullptr);
+	LineTo(_hDC, m_tInfo.fX - 50 + iScrollX, m_tInfo.fY + 300);
+	
+	MoveToEx(_hDC, m_tInfo.fX - 50+ iScrollX, m_tInfo.fY - 300, nullptr);
+	LineTo(_hDC, m_tInfo.fX + 50+ iScrollX, m_tInfo.fY - 300);
+
+	//MoveToEx(_hDC, m_tInfo.fX + 50 + iScrollX, m_tInfo.fY - 300 , nullptr);
+	//LineTo(_hDC, m_tInfo.fX + iScrollX, m_tInfo.fY - 300 + 30);
+	for (int i = 0; i < 10; ++i)
+	{
+		MoveToEx(_hDC, m_tInfo.fX + 50 + iScrollX, m_tInfo.fY - 300 + i*60, nullptr);
+		LineTo(_hDC, m_tInfo.fX + iScrollX, m_tInfo.fY - 300 + 30 + i*60);
+
+		MoveToEx(_hDC, m_tInfo.fX + iScrollX, m_tInfo.fY - 300 + 30 + i * 60, nullptr);
+		LineTo(_hDC, m_tInfo.fX + 50 + iScrollX, m_tInfo.fY - 300 + 60 + i * 60);
+	}
+
+	MoveToEx(_hDC, m_tInfo.fX - 50+ iScrollX, m_tInfo.fY + 300, nullptr);
+	LineTo(_hDC, m_tInfo.fX + 50+ iScrollX, m_tInfo.fY + 300);
+
+
+}
+
+void CThorn::Release(void)
+{
+}
